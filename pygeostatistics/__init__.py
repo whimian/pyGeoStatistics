4876cef5e845352b38764c425248513a357f7ba5
Yu Hao <yuhao89@live.cn>
2017-04-18 21:25:35 +0800

    add two modules to top level __init__

--- pygeostatistics/__init__.py
+++ pygeostatistics/__init__.py
@@ -1,1 +1,3 @@
-from variogram_model import spherical, exponential, gaussian, power, hole_effect
+from .variogram_model import spherical, exponential, gaussian, power, hole_effect
+from .krige3d import Krige3d
+from .sgsim import Sgsim
